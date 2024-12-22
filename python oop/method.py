36d11211198f9dec030d86710c24ebc53647f192
Mahia  Momo <129661915+mahiamOmO@users.noreply.github.com>
2024-12-22 18:57:49 +0600

    Delete python oop directory

--- python oop/method.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self,phy,chem,math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-        self.percentage = str((self.phy+ self.chem+ self.math)/3 + " % ")
-
-stu1 = Student(98,97,99)
-print(stu1.percentage)
--- python oop/metthod_overloading.py
+++
@@ -1,9 +0,0 @@
-class example:
-    def add(self, a, b, c=0):
-        x = a + b + c
-        return x
-
-obj = example()
-
-print(obj.add(10, 20, 30))  # This will output 60
-print(obj.add(10, 20))      # This will output 30 (since c is 0 by default)
